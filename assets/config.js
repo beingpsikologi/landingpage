ece80dc72d7e12f7318ccb07fe975c98a7c2d2ae
beingpsikologi <adminbeing@gmail.com>
2026-08-14 21:52:17 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,7 +3,7 @@
   Setelah Apps Script di-deploy sebagai Web App, tempel URL-nya di bawah.
 */
 window.BEING_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyD82puMnBJXTQcCkSKGBJ1j15LOxKPJPiNOEe6jmE6F8YgJWmkH1lrKRwcC8ZFjKR-/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwhFOhUZI3c9UAgdUulYDCsyktPBPf6gUV0nqClMyCkB_eHHQZGoGvb1jrkMvzCl7Jx/exec",
   CONSULTATION_URL: "konsultasi.html",
   LMS_URL: "https://bangdir.beingpsikologi.com/",
   VOICE_URL: "suara-anda.html",
